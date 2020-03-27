3225f0021764cc5807544930e9714469438d64b5
Mokinz <jz.jakubzolich@gmail.com>
2020-03-27 16:28:39 +0100

    Lel

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-message = "Hello World"
+# # SHORTCUTS
+# ctrl + K + C = comment block
+# ctrl + K + U = uncomment block
 
-print(message)
+
+
+courses = ["History", "Math", "Physics", "CompScience"]
+
+course_str = " - ".join(courses)
+
+new_list = course_str.split(" - ")
+
+
+print(course_str)
+print(new_list)
